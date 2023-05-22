779385bea70550dde840893d8d7dc52bafb22060
Zhi Heng <yapzhhg@gmail.com>
2023-05-22 19:58:00 +0800

    Add redeem commands for star rail and genshin

--- bot/src/cogs/__init__.py
+++ bot/src/cogs/__init__.py
@@ -2,10 +2,12 @@ from .admin import Admin
 from .ping import Ping
 from .notes import Notes
 from .abyss import Abyss
+from .redeem import Redeem
 
 all_cogs = [
     Admin,
     Ping,
     Notes,
     Abyss,
+    Redeem,
 ]
--- bot/src/cogs/redeem.py
+++ bot/src/cogs/redeem.py
@@ -4,7 +4,9 @@ import genshin as gs
 from nextcord import Embed, Colour
 from nextcord.ext import commands
 from nextcord.ext.commands import Bot, Context
-from util import get_all_genshin_accounts_with_token, get_all_star_rail_accounts_with_token
+from util import get_all_genshin_accounts_with_token, get_all_starrail_accounts_with_token
+
+TIME_BETWEEN_REDEEMS = 2.5
 
 class Redeem(commands.Cog):
     def __init__(self, bot: Bot):
@@ -20,6 +22,17 @@ class Redeem(commands.Cog):
             await ctx.reply(embed=embed)
             return
         await self.redeem_code_helper(ctx, code, gs.Game.GENSHIN)
+
+    @commands.command(description="Redeem code for all Star Rail accounts.")
+    async def sredeem(self, ctx: Context, code: str=None):
+        if code == None:
+            embed = Embed(
+                description="Error: please specify a code.\nUsage: $redeem <code>",
+                colour=Colour.brand_red()
+            )
+            await ctx.reply(embed=embed)
+            return
+        await self.redeem_code_helper(ctx, code, gs.Game.STARRAIL)
         
     async def redeem_code_helper(self, ctx: Context, code: str, game_type: gs.Game):
         embed = Embed(
@@ -27,26 +40,34 @@ class Redeem(commands.Cog):
             colour=Colour.brand_green(),
         )
         await ctx.reply(embed=embed)
+
         if game_type == gs.Game.GENSHIN:
             logs = await self.redeem_genshin_code(code)
         else:
-            #TODO Implement star rail code redemption
-            print("TODO")
-        if 'Invalid redemption code' in logs[0]['status']:
+            logs = await self.redeem_starrail_code(code)
+        
+        if len(logs) == 0:
+            embed = Embed(
+                description="No accounts to redeem code for!",
+                colour=Colour.brand_red(),
+            )
+        elif 'Invalid redemption code' in logs[0]['status']:
             embed = Embed(
                 description="Invalid code " + code + " provided!",
                 colour=Colour.brand_red(),
             )
         else:
-            status_list = map(lambda s: f"{s['name']: s['status']}", logs)
+            status_list = map(lambda s: f"{s['name']}: {s['status']}", logs)
             desc = "\n".join(status_list) + "\n\nRedemption completed!"
             embed = Embed(
                 description=desc,
                 colour=Colour.gold(),
             )
+
         await ctx.reply(embed=embed)
 
-    async def redeem_genshin_code(code: str):
+    # TODO Store the redeemed codes in the RedeemedGenshinCode model
+    async def redeem_genshin_code(self, code: str):
         redeemed_users = []
         for acc in get_all_genshin_accounts_with_token():
             redemption_attempt = {
@@ -60,6 +81,32 @@ class Redeem(commands.Cog):
                 "cookie_token": acc.cookie_token,
             }, game=gs.Game.GENSHIN)
             try:
+                await client.redeem_code(code, uid=acc.genshin_uid)
+            except gs.GenshinException as e:
+                redemption_attempt['status'] = str(e)
+            else:
+                redemption_attempt['status'] = "Redeemed!"
+            redeemed_users.append(redemption_attempt)
+            if "Invalid redemption code" in redemption_attempt['status']:
+                return redeemed_users
+            time.sleep(TIME_BETWEEN_REDEEMS)
+        return redeemed_users
+
+    # TODO Store the redeemed codes in the RedeemedStarRailCode model
+    async def redeem_starrail_code(self, code: str):
+        redeemed_users = []
+        for acc in get_all_starrail_accounts_with_token():
+            redemption_attempt = {
+                "name": acc.name,
+                "status": "Not attempted",
+            }
+            client = gs.Client({
+                "ltuid": acc.ltuid,
+                "ltoken": acc.ltoken,
+                "account_id": acc.ltuid,
+                "cookie_token": acc.cookie_token,
+            }, game=gs.Game.STARRAIL)
+            try:
                 await client.redeem_code(code, uid=acc['uid'])
             except gs.GenshinException as e:
                 redemption_attempt['status'] = str(e)
@@ -68,5 +115,5 @@ class Redeem(commands.Cog):
             redeemed_users.append(redemption_attempt)
             if "Invalid redemption code" in redemption_attempt['status']:
                 return redeemed_users
-            time.sleep(2.5)
+            time.sleep(TIME_BETWEEN_REDEEMS)
         return redeemed_users
