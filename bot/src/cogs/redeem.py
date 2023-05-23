764fcf9beb291643863ea08719d81eb16aa8da21
Zhi Heng <yapzhhg@gmail.com>
2023-05-23 10:50:35 +0800

    Add redeemfor commands

--- bot/src/cogs/redeem.py
+++ bot/src/cogs/redeem.py
@@ -4,7 +4,9 @@ import genshin as gs
 from nextcord import Embed, Colour
 from nextcord.ext import commands
 from nextcord.ext.commands import Bot, Context
-from util import get_all_genshin_accounts_with_token, get_all_starrail_accounts_with_token
+
+from models import HoyolabAccount
+from util import get_all_genshin_accounts_with_token, get_all_starrail_accounts_with_token, get_account_by_name
 
 TIME_BETWEEN_REDEEMS = 2.5
 
@@ -21,7 +23,7 @@ class Redeem(commands.Cog):
             )
             await ctx.reply(embed=embed)
             return
-        await self.redeem_code_helper(ctx, code, gs.Game.GENSHIN)
+        await self.redeem_helper(ctx, code, gs.Game.GENSHIN)
 
     @commands.command(description="Redeem code for all Star Rail accounts.")
     async def sredeem(self, ctx: Context, code: str=None):
@@ -32,9 +34,17 @@ class Redeem(commands.Cog):
             )
             await ctx.reply(embed=embed)
             return
-        await self.redeem_code_helper(ctx, code, gs.Game.STARRAIL)
+        await self.redeem_helper(ctx, code, gs.Game.STARRAIL)
+
+    @commands.command(description="Redeem a Genshin code for a specific account.")
+    async def redeemfor(self, ctx: Context, name: str=None, code: str=None):
+        await self.redeem_for_helper(ctx, name, code, gs.Game.GENSHIN)
+    
+    @commands.command(description="Redeem a Star Rail code for a specific account.")
+    async def sredeemfor(self, ctx: Context, name: str=None, code: str=None):
+        self.redeem_for_helper(ctx, name, code, gs.Game.STARRAIL)
         
-    async def redeem_code_helper(self, ctx: Context, code: str, game_type: gs.Game):
+    async def redeem_helper(self, ctx: Context, code: str, game_type: gs.Game):
         embed = Embed(
             description=f"[{game_type}] Redemption in progress...",
             colour=Colour.brand_green(),
@@ -64,6 +74,64 @@ class Redeem(commands.Cog):
                 colour=Colour.gold(),
             )
 
+        await ctx.reply(embed=embed)
+    
+    async def redeem_for_helper(self, ctx: Context, name: str, code: str, game_type: gs.Game):
+        if name == None:
+            embed = Embed(
+                description="Error: please specify a name.\nUsage: $redeemfor <name> <code>",
+                colour=Colour.brand_red()
+            )
+            await ctx.reply(embed=embed)
+            return
+        elif code == None:
+            embed = Embed(
+                description="Error: please specify a code.\nUsage: $redeemfor <name> <code>",
+                colour=Colour.brand_red()
+            )
+            await ctx.reply(embed=embed)
+            return
+
+        acc = get_account_by_name(name)
+        if acc == None:
+            embed = Embed(
+                description="Error: account not found.",
+                colour=Colour.brand_red()
+            )
+        if acc.cookie_token == None:
+            embed = Embed(
+                description="Error: this account does not have a cookie token.",
+                colour=Colour.brand_red()
+            )
+            await ctx.reply(embed=embed)
+            return
+        if acc.genshin_uid == None and game_type == gs.Game.GENSHIN:
+            embed = Embed(
+                description="Error: this account does not have a Genshin uid.",
+                colour=Colour.brand_red()
+            )
+            await ctx.reply(embed=embed)
+            return
+        if acc.starrail_uid == None and game_type == gs.Game.STARRAIL:
+            embed = Embed(
+                description="Error: this account does not have a Star Rail uid.",
+                colour=Colour.brand_red()
+            )
+            await ctx.reply(embed=embed)
+            return
+
+        embed = Embed(
+            description="Redemption in progres...",
+            colour=Colour.brand_green(),
+        )
+        await ctx.reply(embed=embed)
+
+        logs = await self.redeem_code_for_user(acc, code, game_type)
+
+        embed = Embed(
+            description=logs,
+            colour=Colour.gold(),
+        )
         await ctx.reply(embed=embed)
 
     # TODO Store the redeemed codes in the RedeemedGenshinCode model
@@ -117,3 +185,22 @@ class Redeem(commands.Cog):
                 return redeemed_users
             time.sleep(TIME_BETWEEN_REDEEMS)
         return redeemed_users
+
+    async def redeem_code_for_user(self, acc: HoyolabAccount, code: str, game_type: gs.Game):
+        client = gs.Client({
+            "ltuid": acc.ltuid,
+            "ltoken": acc.ltoken,
+            "account_id": acc.ltuid,
+            "cookie_token": acc.cookie_token,
+        }, game=game_type)
+        try:
+            if game_type == gs.Game.GENSHIN:
+                await client.redeem_code(code, uid=acc.genshin_uid)
+            elif game_type == gs.Game.STARRAIL:
+                await client.redeem_code(code, uid=acc.starrail_uid)
+        except gs.GenshinException as e:
+            return str(e)
+        else:
+            return "Redeemed!"
+
+
