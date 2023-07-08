cea836b30e7368692c21ecec6c03b320c54505a2
Zhi Heng <yapzhhg@gmail.com>
2023-07-08 10:20:42 +0800

    Update run script to properly check for .env vars

--- run.sh
+++ run.sh
@@ -13,14 +13,18 @@ fi
 python_version=($(python3 -c 'import sys; print(sys.version_info[0], sys.version_info[1])'))
 required_version=(3 9)
 
+d=$(grep 'DISCORD_TOKEN' ./.env)
+dc=${#d}
+t=$(grep 'TELEGRAM_TOKEN' ./.env)
+tc=${#t}
+
 if (( ${python_version[0]} < ${required_version[0]} || (${python_version[0]} == ${required_version[0]} && ${python_version[1]} < ${required_version[1]}) )); then
     echo "Python3 version 3.9 or higher is required. Terminating script."
     exit 1
-
-elif [ -z "$discord_token" ]; then
+elif [ $dc -eq 14 ]; then
     echo "Discord token not set. Terminating script."
     exit 1
-elif [ -z "$telegram_token" ]; then
+elif [ $tc -eq 15 ]; then
     echo "Telegram token not set. Running Discord bot only..."
     python3 bot/src/app.py
 else
